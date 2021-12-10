9809b45a1be8032fc21118c0cb38dabb4f39752c
RNDIE <57236143+EeZedOh@users.noreply.github.com>
2021-12-10 19:32:22 +0300

    Update IRepository.h

--- IRepository.h
+++ IRepository.h
@@ -1,6 +1,6 @@
 #pragma once
 
 class IRepository {
-    virtual void Open(std::string filename) = 0; // áèíàðíàÿ äåñåðèàëèçàöèÿ â ôàéë
-    virtual void Save() = 0; // áèíàðíàÿ ñåðèàëèçàöèÿ â ôàéë
+    virtual void Open(std::string filename) = 0; 
+    virtual void Save() = 0; 
 };
